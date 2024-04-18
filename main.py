737eb2f45d5e94878d1ef74c1c2a7edc0531940f
VascoPraca <32361310+VascoPraca@users.noreply.github.com>
2024-04-18 09:56:33 +0200

    added function to count letters

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ def main():
     text = get_book_text(book_path)
     print(text)
     count_words(text)
+    count_letters(text)
 
 def get_book_text(path):
     with open(path) as f:
@@ -14,4 +15,11 @@ def count_words(text):
         count += 1
     print(f"Number of words in book: {count}")
     return count
+
+def count_letters(text):
+    letters = {}
+    for letter in text.lower():
+        letters[letter] = letters.get(letter,0)+1
+    print(letters)
+
 main()
