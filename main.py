67851338fb4cd1b51512cd53ceb46a3d0dc635b3
VascoPraca <32361310+VascoPraca@users.noreply.github.com>
2024-04-18 10:21:36 +0200

    sorted report

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 def main():
     book_path = "books/frankenstein.txt"
     text = get_book_text(book_path)
-    print(text)
+    print(f"--- Begin report of {book_path} ---")
     count_words(text)
     count_letters(text)
+    print("--- End report ---")
 
 def get_book_text(path):
     with open(path) as f:
@@ -13,13 +14,14 @@ def count_words(text):
     count = 0
     for word in text.split():
         count += 1
-    print(f"Number of words in book: {count}")
-    return count
+    print(f"{count} words found in the document\n")
 
 def count_letters(text):
     letters = {}
     for letter in text.lower():
         letters[letter] = letters.get(letter,0)+1
-    print(letters)
+    for key, values in letters.items():
+        if key in "abcdefghijklmnopqrstuvwxyz":
+            print(f"The '{key}' character was found {values} times")
 
 main()
